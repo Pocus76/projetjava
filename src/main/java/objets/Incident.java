39f190a399fca15e095482f953023d0740c949e7
Gregoire <GuillaumatG@hotmail.fr>
2019-03-22 15:55:06 +0100

    Fenetre incident

--- src/main/java/objets/Incident.java
+++ src/main/java/objets/Incident.java
@@ -1,6 +1,16 @@
 package objets;
 
+import mysqlUtil.SqlConnexion;
+import util.Constants;
+import util.LogUtils;
+
+import javax.swing.*;
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Date;
 import java.util.List;
 
@@ -9,17 +19,26 @@ public class Incident
     private BigInteger incident_id;
     private Personne personne;
     private Administratif administratif;
-    private List<SuperCivil> villains;
+    private List<SuperCivil> vilains;
     private String commentaire;
     private Date creation_date;
     private Boolean sans_suite;
 
-    public Incident(BigInteger incident_id, Personne personne, Administratif administratif, List<SuperCivil> villains, String commentaire, Date creation_date, Boolean sans_suite)
+    public Incident(Administratif administratif, List<SuperCivil> vilains, String commentaire) {
+        this.administratif = administratif;
+        this.personne = Constants.utilisateurConnecte;
+        this.vilains = vilains;
+        this.commentaire = commentaire;
+        this.creation_date = new Date();
+        this.sans_suite = false;
+    }
+
+    public Incident(BigInteger incident_id, Personne personne, Administratif administratif, List<SuperCivil> vilains, String commentaire, Date creation_date, Boolean sans_suite)
     {
         this.incident_id = incident_id;
         this.personne = personne;
         this.administratif = administratif;
-        this.villains = villains;
+        this.vilains = vilains;
         this.commentaire = commentaire;
         this.creation_date = creation_date;
         this.sans_suite = sans_suite;
@@ -49,7 +68,48 @@ public class Incident
         return sans_suite;
     }
 
-    public List<SuperCivil> getVillains() {
-        return villains;
+    public List<SuperCivil> getVilains() {
+        return vilains;
+    }
+
+    public Boolean insertIntoDatabase() {
+        boolean inserted = false;
+
+        String query = "INSERT INTO INCIDENTS (ADMINISTRATIF_ID, PERSONNE_ID, COMMENTAIRE, CREATAION_DATE, SANS_SUITE) VALUES (?, ?, ?, ?, ?);";
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = SqlConnexion.connection.prepareStatement(query);
+            stmt.setBigDecimal(1, new BigDecimal(administratif.getAdministratif_id()));
+            stmt.setBigDecimal(2, new BigDecimal(personne.getPersonne_id()));
+            stmt.setString(3, commentaire);
+            stmt.setDate(4, new java.sql.Date(creation_date.getTime()));
+            stmt.setBoolean(5, sans_suite);
+            stmt.execute();
+
+            Statement stmtId = SqlConnexion.connection.createStatement();
+            String queryId = "SELECT LAST_INSERT_ID() AS INCIDENT_ID";
+            ResultSet rs = stmtId.executeQuery(queryId);
+            rs.next();
+            incident_id = BigInteger.valueOf(rs.getInt("INCIDENT_ID"));
+
+            if (vilains.size() > 0) {
+                for (int i = 0; i < vilains.size(); i++) {
+                    SuperCivil vilain = vilains.get(i);
+                    String queryVilains = "INSERT INTO INCIDENTS_VILAINS (INCIDENT_ID, SUPER_CIVIL_ID) VALUES (?, ?);";
+                    PreparedStatement stmt2 = SqlConnexion.connection.prepareStatement(queryVilains);
+                    stmt2.setBigDecimal(1, new BigDecimal(administratif.getAdministratif_id()));
+                    stmt2.setBigDecimal(2, new BigDecimal(vilain.getSuperCivil_id()));
+                    stmt2.execute();
+                }
+            }
+
+            inserted = true;
+        } catch (SQLException e) {
+            LogUtils.logErreur(this.getClass().getSimpleName(), e.getMessage());
+            JOptionPane.showMessageDialog(null, "Une erreur est survenue", "Erreur", JOptionPane.ERROR_MESSAGE);
+        }
+
+        return inserted;
     }
 }

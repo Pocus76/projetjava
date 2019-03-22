32a3e8a6a75d67b8615e92cec441e20ec2fc3a22
Gregoire <GuillaumatG@hotmail.fr>
2019-03-22 09:16:43 +0100

    Administratif et personne

---
+++ src/main/java/objets/Administratif.java
@@ -0,0 +1,51 @@
+package objets;
+
+import java.math.BigInteger;
+
+public class Administratif {
+    private BigInteger administratif_id;
+    private String adresse;
+    private String cp;
+    private String ville;
+    private String pays;
+    private Planete planete;
+    private String telephone;
+    private String email;
+
+    public Administratif(BigInteger administratif_id, String adresse, String cp, String ville, String pays, Planete planete, String email, String telephone) {
+        this.administratif_id = administratif_id;
+        this.adresse = adresse;
+        this.cp = cp;
+        this.ville = ville;
+        this.pays = pays;
+        this.planete = planete;
+        this.telephone = telephone;
+        this.email = email;
+    }
+
+    public BigInteger getAdministratif_id() { return administratif_id; }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getCp() {
+        return cp;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public Planete getPlanete() {
+        return planete;
+    }
+
+    public String getTelephone() { return  telephone; }
+
+    public String getEmail() { return email; }
+}
